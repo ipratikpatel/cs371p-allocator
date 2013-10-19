312ac93c0149c3891b0a84695cce59594a73e453
Mike Carrillo <michael.carrillo@utexas.edu>
2013-10-19 14:05:07 -0500

    Got deallocate working

--- Allocator.h
+++ Allocator.h
@@ -3,10 +3,10 @@
 // Copyright (C) 2013
 // Glenn P. Downing
 // ------------------------------
-
+ 
 #ifndef Allocator_h
 #define Allocator_h
-
+ 
 // --------
 // includes
 // --------
@@ -154,7 +154,7 @@ class Allocator {
                         {
                             free_block_size = *sent_p;
                             *sent_p = num_bytes_allocating * -1;
-                            alloc_p = (pointer) (sent_p + 1);
+                            alloc_p = reinterpret_cast<pointer>(sent_p + 1);
                             temp_a += num_bytes_allocating + 4; 
                             sent_p = (int *) temp_a;
                             *sent_p = num_bytes_allocating * -1;
@@ -173,7 +173,7 @@ class Allocator {
                             // check if (*sent_p - num_bytes_allocating) == 0 then give it all
                             free_block_size = *sent_p;
                             *sent_p *= -1;
-                            alloc_p = (pointer) (sent_p + 1);
+                            alloc_p = reinterpret_cast<pointer>(sent_p + 1);
                             temp_a += free_block_size + 4; 
                             sent_p = (int *) temp_a;
                             *sent_p *= -1;    
@@ -227,7 +227,61 @@ class Allocator {
          */
         void deallocate (pointer p, size_type) {
             // <your code>
-            assert(valid());}
+            assert(valid());
+	    int* int_p = reinterpret_cast<int*>(p);
+
+	    int* block1 = int_p - 1;
+	    char* temp_block1 = reinterpret_cast<char*>(int_p);
+	    int* block2 = reinterpret_cast<int*>(temp_block1 + (*block1 * -1));
+	    int* adj_block1;
+	    int* adj_block2;
+	    int* lb;
+	    int* rb;
+
+	    assert(*block1 < 0);
+	    assert(*block2 < 0);
+
+	    if(block1 == reinterpret_cast<int*>(a)){
+	        adj_block1 = NULL;
+	    }
+	    else{
+		adj_block1 = block1 - 1;
+	    }
+
+	    if(block2 + 1 == reinterpret_cast<int*>(a+N)){
+		adj_block2 = NULL;
+	    }
+	    else{
+		adj_block2 = block2 + 1;
+	    }
+
+	    if(adj_block1 != NULL && *adj_block1 > 0){
+		int bytes_adj_block1 = *adj_block1;
+		char* temp_adj_block1 = reinterpret_cast<char*>(adj_block1);
+		lb = reinterpret_cast<int*>(temp_adj_block1 - bytes_adj_block1 - 4);
+		*lb = (-1 * *block1) + *adj_block1 + 8;
+		*block1 = 0;
+		*adj_block1 = 0;
+	    }
+	    else{
+		lb = block1;
+		*lb = *block1 * -1;
+	    }
+
+	    if(adj_block2 != NULL && *adj_block2 > 0){
+		int bytes_adj_block2 = *adj_block2;
+		char* temp_adj_block2 = reinterpret_cast<char*>(adj_block2);
+		rb = reinterpret_cast<int*>(temp_adj_block2 + bytes_adj_block2 + 4);
+		*rb = bytes_adj_block2 + *lb + 8;
+		*lb = *rb;
+		*block2 = 0;
+		*adj_block2 = 0;
+	    }
+	    else{
+		rb = block2;
+		*rb = *lb;
+	    }
+	}
 
         // -------
         // destroy
